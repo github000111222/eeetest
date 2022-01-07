4b890ca3db730bf7a87ea0fdd1d30b6862c8611d
github000111222 <96434435+github000111222@users.noreply.github.com>
2022-01-07 12:38:53 +0800

    Update eeetest.sh

--- eeetest.sh
+++ eeetest.sh
@@ -233,7 +233,7 @@ preinstall_l2tp(){
 
     password=`rand`
     echo "Please enter ${username}'s password:"
-    tmppassword="123456"
+    tmppassword="pTx@48#gf69"
     [ ! -z ${tmppassword} ] && password=${tmppassword}
 
     echo
